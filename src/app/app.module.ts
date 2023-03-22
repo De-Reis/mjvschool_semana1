9cf1ca0b7e6a52bfdf8b67e2dff6314b42a97e5e
Denise Reis <dederh.reis@gmail.com>
2023-03-22 21:40:14 +0000

    criacao componentes header e footer exercicio 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PrincipalComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
